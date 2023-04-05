a9f781a8a8001b62834e96134c9591a406efe110
MentorUsama <usama.farhat.99@gmail.com>
2023-04-06 03:10:31 +0500

    wip

--- src/ContinueWithApple/ContinueWithApple.jsx
+++ src/ContinueWithApple/ContinueWithApple.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import AppleLogin from 'react-apple-login';
 import * as styles from './styles';
 import axios from 'axios';
 
 // https://appleid.apple.com/auth/authorize?response_type=code%20id_token&client_id=com.tandemexperiences.login.app&redirect_uri=https://sample-react-app-vercel-testing.vercel.app/&state=aa5b531d39&scope=name%20email&response_mode=form_post
 const ContinueWithApple = ({ appleResponse }) => {
-  console.log("version 1")
+  const params = new URLSearchParams(window.location.search);
   const onResponseReceived = async (data) => {
     console.log("data v1",data)
     if(!data.code) 
@@ -19,6 +19,14 @@ const ContinueWithApple = ({ appleResponse }) => {
       console.log("error",e)
     }
   };
+
+  useEffect(() => {
+    let apple_auth = params.get('appletoken');
+    if (!apple_auth) return;
+    const decodedAcces=decodeURI(apple_auth)
+    console.log('apple_auth found', decodedAcces);
+  }, []);
+
   return (
     <AppleLogin
       clientId="com.tandemexperiences.login.app"
